4960c9539bb53861fa2e1539d30f810421f42c59
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-08-28 22:41:30 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,11 @@
+public class even {
+    public static void main(String[] args) {
+        int array[]={10,35,56,75,20};
+        for(int i=0;i<array.length;i++){
+        if(array[i] % 2==0){
+            System.out.println("the even numbers in array are:"+ array[i]);
+            
+         }
+    }
+} 
+}
