d174b8785d45bd97960d154dfd9b4551831e89d1
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-08-28 22:41:59 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,12 @@
+public class odd {
+    public static void main(String[]args){
+        int array[]={23,56,20,67,46};
+        for(int i=0;i<array.length;i++){
+            if(array[i] %2 != 0){
+                System.out.println("the odd numbers in the array are:"+ array[i]);
+            }
+        }
+    }
+    
+}
+
