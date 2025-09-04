b07dc326a9784fd8230d37798d5493084352beb2
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-04 22:09:37 +0530

    Add files via upload

---
+++ arr1.java
@@ -0,0 +1,16 @@
+
+public class arr1 {
+    public static void main(String[]args){
+        int[] arr={10,20,30,80,90};
+        int count=0;
+        for(int num:arr){
+            if(num>50){
+                count++;
+            }
+            System.out.println("numbers greater than 50 in array are:"+count);
+            
+            }
+        }
+    }
+    
+
