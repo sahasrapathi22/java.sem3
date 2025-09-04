a75788b7a177d0e58ee4af7b919aa58b50e509a2
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-04 22:12:32 +0530

    Add files via upload

---
+++ arr2.java
@@ -0,0 +1,11 @@
+public class arr2 {
+    public static void main(String[]args){
+        int[]arr={10,20,40,50};
+        System.out.println("array in the reverse order:");
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.println(arr[i]);
+
+        }
+    }
+    
+}
