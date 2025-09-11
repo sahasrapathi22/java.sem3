a20d9ad7feb51e5d8632a26364ea950e7f72829e
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-11 21:21:02 +0530

    Add files via upload

---
+++ LogicandExample.java
@@ -0,0 +1,19 @@
+public class LogicandExample {
+    public static void main(String[] args) {
+
+int age = 25;
+
+boolean hasLicense = true;
+
+if (age > 18 && hasLicense) {
+
+System.out.println("Eligible to drive.");
+
+} else {
+
+System.out.println("Not eligible to drive.");
+
+}
+}
+    
+}
