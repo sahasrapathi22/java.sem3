c3a5055ef55839776057241cdd1be33f5740e4c7
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-11 21:19:33 +0530

    Add files via upload

--- greater.java
+++ greater.java
@@ -1,17 +1,7 @@
-
-public class arr1 {
-    public static void main(String[]args){
-        int[] arr={10,20,30,80,90};
-        int count=0;
-        for(int num:arr){
-            if(num>50){
-                count++;
-            }
-            System.out.println("numbers greater than 50 in array are:"+count);
-            
-            }
-        }
+public class greater{
+    public static void main(String[] args) {
+        int x=15,y=10;
+        System.out.println("x>y :"+(x>y));
+        System.out.println("x<y :"+(x<y));
     }
-    
-
-
+}
