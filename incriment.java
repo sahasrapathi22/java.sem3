a21cb70d7c77abfa93199e0906cd92c061255b52
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-11 21:19:09 +0530

    Add files via upload

---
+++ incriment.java
@@ -0,0 +1,13 @@
+public class incriment {
+    public static void main(String[] args) {
+        int x=30;
+        System.out.println("original:"+x);
+        System.out.println("post incriment:"+(x++));
+        System.out.println("after post incriment:"+x);
+        System.out.println("pre incrimennt :"+(++x));
+        System.out.println("post decrement:"+(x--));
+        System.out.println("pre incriment:"+(--x));
+
+    }
+    
+}
