6a3e109407336183fd8ff99b382b64b4eb2592ab
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-11 21:14:43 +0530

    Add files via upload

---
+++ and.java
@@ -0,0 +1,16 @@
+public class and {
+    public static void main(String[] args) {
+        int a=12;
+        int b=10;
+        int result=a&b;
+        int result1=a|b;
+        int result2=a^b;
+        int result3=~a;
+        System.out.println("a&b value is = "+result);
+        System.out.println("a|b value is = "+result1);
+        System.out.println("a^b value is = "+result2);
+        System.out.println("~a value is = "+result3);
+
+    }
+    
+}
