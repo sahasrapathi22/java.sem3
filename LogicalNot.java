625157a426d30bbea5c4f28e7d2830fa63f936d3
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-11 21:21:22 +0530

    Add files via upload

---
+++ LogicalNot.java
@@ -0,0 +1,17 @@
+public class LogicalNot {
+    public static void main(String[] args) {
+
+boolean isRaining = false;
+
+if (!isRaining) {
+
+System.out.println("You can go outside without an umbrella.");
+
+} else {
+
+System.out.println("Take an umbrella!");
+    
+}
+    
+    }
+}
