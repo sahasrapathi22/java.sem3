29588ee37bc214e0408ec38ede5fc806eb2f499e
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-11 21:21:43 +0530

    Add files via upload

---
+++ StuandTea.java
@@ -0,0 +1,12 @@
+public class StuandTea {
+    public static void main(String[] args) {
+      boolean isStudent = true;
+      boolean isTeacher = false; 
+      if (isStudent || isTeacher) {
+          System.out.println("Access granted.");
+      } else {
+          System.out.println("Access denied.");
+      }     
+    }
+    
+}
