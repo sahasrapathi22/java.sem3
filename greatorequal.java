c743fd3816a8ed5171037d1acc598f5d8c8308a5
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-11 21:20:01 +0530

    Add files via upload

---
+++ greatorequal.java
@@ -0,0 +1,9 @@
+public class greatorequal {
+    public static void main(String[] args) {
+        int x=15,y=10;
+        System.out.println("x>=y :"+(x>=y));
+        System.out.println("x<=y :"+(x<=y));
+    }
+}
+    
+
