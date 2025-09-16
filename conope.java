7c97ae11fcc6d3123d276b5b11cbf2f042acb43b
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-16 18:39:59 +0530

    Add files via upload

---
+++ conope.java
@@ -0,0 +1,8 @@
+public class conope {
+    public static void main(String[] args) {
+        int number=27;
+        String result=(number%2==0)?"even":"odd";
+        System.out.println("the number "+ number +" is "+ result);
+    }
+    
+}
