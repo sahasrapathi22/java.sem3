5b5b7ac4dd50f11cfe2fe26bc6a4a149f184977f
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-16 18:42:01 +0530

    Add files via upload

---
+++ prefix.java
@@ -0,0 +1,9 @@
+public class prefix {
+    public static void main(String []args){
+        int a=10, b=6;
+        int result=++a*b+2;
+        System.out.println("result is: "+result);
+
+    }
+    
+}
