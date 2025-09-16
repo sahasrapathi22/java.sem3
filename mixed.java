f5fd8259db83fc773f72fb13eb451a5d35e9165f
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-16 18:41:29 +0530

    Add files via upload

---
+++ mixed.java
@@ -0,0 +1,8 @@
+public class mixed {
+    public static void main(String[] args) {
+        int a=6,b=7,c=4;
+        int result=++a*(b--+~c)-(-b);
+        System.out.println("rersult is : "+result);
+        
+    }
+}
