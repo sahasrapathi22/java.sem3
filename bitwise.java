d96526eb8cbc9f2078357d1394263d23e97fdbba
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-16 18:40:19 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,8 @@
+public class bitwise {
+    public static void main(String[] args) {
+        int a=9;
+        int result=(~a+1)*2;
+        System.out.println("result is: "+result);
+    }
+    
+}
