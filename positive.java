91ee8ff16b12d3587544ebe5c991823393171149
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-09-16 18:43:09 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,9 @@
+public class positive {
+    public static void main(String[] args) {
+        int num=5;
+        String result=(num>0)?"positive":(num<0)?"negative":"zero";
+        System.out.println("the number "+ num + " is 3333" +result);
+
+    }
+    
+}
