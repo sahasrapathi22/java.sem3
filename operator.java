2ce7d1d063e66f27ba8eb2eb474961f9d552cb02
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-11-13 22:38:41 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,7 @@
+public class operator {
+    public static void main(String[] args) {
+        int result1=10+7-3*2;
+        System.out.println("result without parentheses : "+ result1);
+    }
+    
+}
