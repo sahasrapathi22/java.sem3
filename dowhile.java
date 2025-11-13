fcab3e8421489e1f4575204a3b0cb5c823ac9ff4
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-11-13 22:36:37 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,9 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int i=1;
+        do { 
+            System.out.println("value:" +i);
+            i++;
+    }while(i<=5);
+}
+}
