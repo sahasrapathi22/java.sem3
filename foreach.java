717f125f82f7156d7a70b667ab076e9b36ba6795
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-11-13 22:37:13 +0530

    Add files via upload

---
+++ foreach.java
@@ -0,0 +1,8 @@
+public class foreach {
+    public static void main(String[] args) {
+        String[] fruits={"apples","cherry","fig"};
+        for(String fruit:fruits){ 
+            System.out.println("fruit:" +fruit);
+        }
+    }
+}
