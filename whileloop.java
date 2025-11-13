fc7732b2c03f058b3db60350d732a8e62305caa4
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-11-13 22:45:37 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,13 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int number=123456;
+        int sum=0;
+        while(number>0){
+            int digit=number%10;
+            sum+=digit;
+            number=number/10;
+        }
+        System.out.println("sum of digits=" +sum);
+    }
+    
+}
