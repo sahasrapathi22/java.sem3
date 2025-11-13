f3495c3d5b27213ab90e839b39b339210fea82da
sahasrapathi22 <sahasrapathi22@gmail.com>
2025-11-13 22:30:54 +0530

    Add files via upload

---
+++ breakexample.java
@@ -0,0 +1,12 @@
+public class breakexample {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+        if(i==5){
+            System.out.println("breaking the loop at i=" +i);
+            break;
+
+        }
+        System.out.println("i=" +i);
+    }
+}
+}
